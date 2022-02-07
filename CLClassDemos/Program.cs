66ddddf88099f09b95cbad030131823f498beca9
Ashely Arnold <AArnold9591@gmail.com>
2022-02-07 16:05:21 -0500

    test

--- CLClassDemos/Program.cs
+++ CLClassDemos/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
-
-namespace CLClassDemos
+namespace StaticQuiz
 {
+    public class TestClass
+    {
+        // static property with initial value of 0
+        public static int StaticProperty { get; set; } = 0;
+        // non-static property with initial value of 0
+        public int NonStaticProperty { get; set; } = 0;
+        // every time we create an instance of TestClass
+        // increment the StaticProperty AND the NonStaticProperty
+        public TestClass()
+        {
+            StaticProperty++;
+            NonStaticProperty++;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TestClass test = null;
+            for (int i = 0; i < 10; i++)
+            {
+                test = new TestClass();
+            }
+            Console.WriteLine($"test.NonStaticProperty = {test.NonStaticProperty}");
+            Console.WriteLine($"Test.StaticProperty = {TestClass.StaticProperty}");
         }
     }
 }
