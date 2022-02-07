20bc1b76b5ad88fbc48cbbe6a33652a8f8e71f57
Ashely Arnold <AArnold9591@gmail.com>
2022-02-07 15:26:40 -0500

    added demo app solution

---
+++ CLClassDemos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CLClassDemos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
